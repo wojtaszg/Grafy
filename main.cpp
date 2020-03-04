c4c2c201d64f164dda3132b1b28c390588077928
Wojtek Ratajczak <49553631+wojtaszg@users.noreply.github.com>
2020-03-04 23:31:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,72 +6,45 @@
 
 using namespace std;
 
+int main()
+{
+	int v1,v2,v ,p;
+	graf *tab[100];
+	int n;
+	cout << "Podaj rozmiar grafu: ";
+	cin >> n;
+	cout << "Podaj gestosc grafu: ";
+	cin >> p;
+	cout << "Podaj wierzcholek poczatkowy: ";
+	cin >> v;
 
 
-		int main()
-		{
-			int v1,v2,v ,p;
+	for (int i = 0; i < 100; i++)
+	{
+		tab[i] = new graf(n);
+		tab[i]->poczatek(n, v);
+		tab[i]->wypelnijMacierz(n, p);
+	}
 			
-			/*cout<<"Podaj wierzcholek startowy i koncowy: ";
-			cin >> v1 >> v2;
-			cout<<"Podaj liczbe wierzcholkow i gestosc: ";
-			cin>>v >> p;
-			graf a(v);
-			a.poczatek(v,v1);
-			a.wypelnijLosowo(v,p);
-			a.wyznaczSciezki(v);
-			a.wyswietlWynikNajDrogi(v,v2);
-			a.zapiszWynikiSciezki(v,v2);
-			//a.najkrotszaDrogaMiedzyWierzcholkami(0, 3,10);*/
-		
-					cout<<"Macierz: "<<endl;
-					graf a(6);
-					a.poczatek(6, 0);
-					a.wczytajGraf('m');
-					a.wyznaczSciezkiMacierz(6);
-					a.wyswietlWynikMacierzy(6);
+	double start = clock();
+	for (int j = 0; j < 100; j++)
+		tab[j]->wyznaczSciezkiMacierz(n);
+	double czas = (clock() - start) / CLOCKS_PER_SEC;
 
-		
-
+	cout << "Czas dla 100 grafow: " << czas << endl;
+	cout << "Sredni czas dla 1 grafu: " << czas / 100 << endl;
+			cout << "---------------------" << endl;
+	cout<<"Podaj wierzcholek startowy i koncowy: ";
+	cin >> v1 >> v2;
+	cout<<"Podaj liczbe wierzcholkow i gestosc: ";
+	cin>>v >> p;
+	graf a(v);
+	a.poczatek(v,v1);
+	a.wypelnijLosowo(v,p);
+	a.wyznaczSciezki(v);
+	a.wyswietlWynikNajDrogi(v,v2);
+	a.zapiszWynikiSciezki(v,v2);
 			
-
-			/*graf *tab[100];
-				int n;
-			cout << "Podaj rozmiar grafu: ";
-			cin >> n;
-			cout << "Podaj gestosc grafu: ";
-			cin >> p;
-			cout << "Podaj wierzcholek poczatkowy: ";
-			cin >> v;
-
-
-				for (int i = 0; i < 100; i++)
-				{
-					tab[i] = new graf(n);
-					tab[i]->poczatek(n, v);
-					tab[i]->wypelnijMacierz(n, p);
-				}
-				double start = clock();
-				for (int j = 0; j < 100; j++)
-					tab[j]->wyznaczSciezkiMacierz(n);
-				double czas = (clock() - start) / CLOCKS_PER_SEC;
-
-				cout << "Czas dla 100 grafow: " << czas << endl;
-				cout << "Sredni czas dla 1 grafu: " << czas / 100 << endl;*/
-				/*graf a(6);
-					a.poczatek(6, 0);
-					a.wczytajGraf('l');
-					a.wyznaczSciezki(6);
-					a.wyswietlWynik(6);
-					cout << "Czy chcesz zapisac wynik? \n1 - tak\n2 - nie\n";
-					cin >> x;
-					if (x == 1)
-						a.zapiszWyniki(6);*/
-
-
-
-			int c;
-			cin >> c;
-			return 0;
-		}
+	return 0;
+}
 	
